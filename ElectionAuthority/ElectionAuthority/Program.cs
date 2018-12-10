b6e7f6baf26ad8b57538663f585bcf9d23c3b190
cinnamonade <pwojslawp@gmail.com>
2018-12-10 10:31:35 +0100

    test

--- ElectionAuthority/ElectionAuthority/Program.cs
+++ ElectionAuthority/ElectionAuthority/Program.cs
@@ -17,6 +17,7 @@ namespace ElectionAuthority
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+			// testowy komentarz
         }
     }
 }
